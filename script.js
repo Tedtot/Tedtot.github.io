7b1176f12b5a8f932ae01e488131caad2410cb24
Tedtot <127804021+Tedtot@users.noreply.github.com>
2023-06-04 11:19:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 var garbage = document.getElementsByClassName("garbage");
 var recycle = document.getElementsByClassName("recycle");
+var compost = document.getElementsByClassName("compost");
+
+var garbageUI = 
+    [
+      "https://clipart-library.com/images_k/cup-of-lean-transparent/cup-of-lean-transparent-22.png",
+      "https://static.thenounproject.com/png/1359370-200.png",
+      "https://clipart-library.com/data_images/463488.png",
+      "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/33592/potato-chips-clipart-xl.png",
+      "https://clipartix.com/wp-content/uploads/2016/05/Hot-coffee-cup-clipart-image.png"
+    ];
+var recycleUI = 
+    [
+      "https://clipart-library.com/img/1856864.png",
+      "https://clipartix.com/wp-content/uploads/2016/04/Primary-writing-paper-clipart.png",
+      "https://www.freepnglogos.com/uploads/box-png/box-png-transparent-google-objects-pinterest-9.png",
+      "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/23624/soda-can-clipart-xl.png",
+      "https://www.pngmart.com/files/16/Translucent-Glass-Bottle-PNG-Clipart.png"
+    ];
+var compostUI = 
+    [
+      "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/59740/pizza-box-clipart-md.png",
+      "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/7821764/tree-branch-clipart-xl.png",
+      "https://www.pngkit.com/png/full/293-2931363_shell-clipart-transparent-background-cracked-egg-shell-clipart.png",
+      "https://github.com/Tedtot/Tedtot.github.io/banana.png",
+      "https://www.freepnglogos.com/uploads/plate-png/plate-aspen-paper-products-25.png"
+    ];
 
 var gameButton = document.getElementById("gamebutton");
-var bins = [document.getElementById("trashBin"), document.getElementById("recycleBin")];
+var bins = [document.getElementById("trashBin"), document.getElementById("recycleBin"), 
+            document.getElementById("compostBin")];
 var pointText = document.getElementById("points");
 var timer = document.getElementById("timer");
 var gameActive = false;
@@ -17,9 +44,10 @@ function startGame() {
   points = 0;
   pointText.innerHTML = "Points: 0";
   
-  for (var i = 1; i < garbage.length; i++) { console.log(garbage.length); garbage[i].remove(); }
-  for (var i = 1; i < recycle.length; i++) recycle[i].remove();
-  
+  for (var i = garbage.length - 1; i > 0; i--) garbage[i].remove(); 
+  for (var i = recycle.length - 1; i > 0; i--) recycle[i].remove();
+  for (var i = compost.length - 1; i > 0; i--) compost[i].remove();
+
   if (gameActive) {
     endGame(false);
     return;
@@ -32,18 +60,30 @@ function startGame() {
   
   for (var i = 0; i < Math.floor(Math.random() * 8) + 5; i++) {
     var clone;
-    if (Math.random() < 0.5) {
+    var ui;
+    var rand = Math.random() * 3;
+    if (rand < 1) {
       clone = garbage[0].cloneNode(true);
+      ui = garbageUI[Math.floor(Math.random() * garbageUI.length)];
       garbage[0].parentNode.appendChild(clone);
     }
-    else {
+    
+    else if (rand < 2){
       clone = recycle[0].cloneNode(true);
+      ui = recycleUI[Math.floor(Math.random() * recycleUI.length)];
       recycle[0].parentNode.appendChild(clone);
     }
-    clone.style.top = Math.round(Math.random() * document.body.offsetHeight + 150) + 'px';
+    
+    else {
+      clone = compost[0].cloneNode(true);
+      ui = compostUI[Math.floor(Math.random() * compostUI.length)];
+      compost[0].parentNode.appendChild(clone);
+    }
+
+    clone.style.top = Math.round(Math.random() * document.body.offsetHeight + 250) + 'px';
     clone.style.left = Math.round(Math.random() * document.body.offsetWidth) + 'px';
-  
     clone.style.opacity = 1;
+    clone.src = ui;
     dragElement(clone);
   }
 }
@@ -116,11 +156,12 @@ function doAction(element, bin) {
      changePoints(5);
   else if (binID == "recycleBin" && elementClass == "recycle")
      changePoints(10);
-
+  else if (binID == "compostBin" && elementClass == "compost")
+     changePoints(8);
   else changePoints(-8);
   
   pointText.innerHTML = "Points: " + points;
-  if (garbage.length <= 1 && recycle.length <= 1) endGame(true);
+  if (garbage.length <= 1 && recycle.length <= 1 && compost.length <= 1) endGame(true);
 }
 
 function changePoints(amount) {
